b11e7e977d754568f6f4053a47013e270f5a4416
Halyna Moldovianu <moldovianuhalyna@gmail.com>
2025-09-17 09:27:23 +0200

    tailwind added

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,17 @@
+import FilterSideBar from "../../components/FilterSideBar/FilterSideBar";
+import Logo from "../../components/Logo/Logo";
+import ProductCatalog from "../../components/ProductCatalog/ProductCatalog";
+import SearchBar from "../../components/SearchBar/SearchBar";
+
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div className="flex items-center bc-background ">
+      <Logo />
+      <FilterSideBar />
+      <SearchBar />
+      <ProductCatalog />
+    </div>
+  );
 };
 
 export default Homepage;
